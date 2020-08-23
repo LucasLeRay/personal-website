6cb6fcd2c939cc5e13ce514f310c680d0abef293
Lucas Le Ray <contact@lucas-le-ray.com>
2020-08-23 23:53:20 +0200

    feat: add dark mode toggle

--- src/components/ColorModeToggle/index.js
+++ src/components/ColorModeToggle/index.js
@@ -1,11 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+
 import { ThemeContext } from '../ThemeProvider'
+import { Container } from './ColorModeToggle.module.css'
+import Moon from './moon.svg'
 
 function ColorModeToggle () {
   const { colorMode, setColorMode } = useContext(ThemeContext)
+  const [ toggle, setToggle ] = useState(0)
 
   return (
-    <button style={{backgroundColor: 'blue'}} onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>{colorMode === 'dark' ? '🌙' : '☀️'}</button>
+    <div className={Container}>
+      <Moon
+        onClick={() => {
+          setColorMode(colorMode === 'light' ? 'dark' : 'light')
+          setToggle(1)
+        }}
+        onAnimationEnd={() => setToggle(0)}
+        toggle={toggle}
+      />
+    </div>
   )
 }
 
