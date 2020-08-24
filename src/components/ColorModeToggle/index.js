b4cf5d63006911cdacbf6234050a865d42794530
Lucas Le Ray <contact@lucas-le-ray.com>
2020-08-25 01:15:42 +0200

    feat: added sun to colormode toggle

--- src/components/ColorModeToggle/index.js
+++ src/components/ColorModeToggle/index.js
@@ -3,21 +3,42 @@ import React, { useContext, useState } from 'react'
 import { ThemeContext } from '../ThemeProvider'
 import { Container } from './ColorModeToggle.module.css'
 import Moon from './moon.svg'
+import Sun from './sun.svg'
 
 function ColorModeToggle () {
   const { colorMode, setColorMode } = useContext(ThemeContext)
-  const [ toggle, setToggle ] = useState(0)
+  const [ toggle, setToggle ] = useState('IDDLE')
 
   return (
     <div className={Container}>
-      <Moon
+      {colorMode === 'dark' && (<Moon
         onClick={() => {
-          setColorMode(colorMode === 'light' ? 'dark' : 'light')
-          setToggle(1)
+          setToggle('OUT')
         }}
-        onAnimationEnd={() => setToggle(0)}
+        onAnimationEnd={() => {
+          if (toggle === 'OUT') {
+            setColorMode('light')
+            setToggle('IN')
+          } else {
+            setToggle(null)
+          }
+        }}
         toggle={toggle}
-      />
+      />)}
+      {colorMode === 'light' && (<Sun
+        onClick={() => {
+          setToggle('OUT')
+        }}
+        onAnimationEnd={() => {
+          if (toggle === 'OUT') {
+            setColorMode('dark')
+            setToggle('IN')
+          } else {
+            setToggle(null)
+          }
+        }}
+        toggle={toggle}
+      />)}
     </div>
   )
 }
