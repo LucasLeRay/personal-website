06efe3f3481ba7a5722b7d567266448aaed8e836
Lucas Le Ray <contact@lucas-le-ray.com>
2020-09-10 23:02:09 +0200

    fix: update db secret for post like

---
+++ functions/post-likes.js
@@ -0,0 +1,39 @@
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+const dbClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+const limit = process.env.LIKE_LIMIT
+
+exports.handler = async function ({ httpMethod, body }, context, callback) {
+  const { id, userId, count } = JSON.parse(body);
+  if (httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  } else if (!id || !userId || isNaN(count) || count > limit) {
+    return { statusCode: 400, body: 'Invalid parameter' }
+  }
+  
+  try {
+    const res = await dbClient.query(
+      q.If(
+        q.Exists(q.Match(q.Index('likes_by_user_id'), [ id, userId ])),
+        q.Update(
+          q.Select(['ref'], q.Get(q.Match(
+            q.Index("likes_by_user_id"), [ id, userId ]
+          ))),
+          { data: { count } }
+        ),
+        q.Create(q.Collection('likes'), { data: { id, userId, count } })
+      )
+    )
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message
+    }
+  }
+}
