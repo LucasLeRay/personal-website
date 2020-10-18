1434ed788b2e9729248f6c78d863c9c6027ac4c1
LucasLeRay <contact@lucas-le-ray.com>
2020-10-18 22:43:27 +0200

    fix: social image path

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -10,42 +10,42 @@ function Social() {
         href="https://twitter.com/Lucas_Le_Ray"
         rel="noopener noreferrer"
       >
-        <img src="img/twitter.png" alt="twitter" />
+        <img src="/img/twitter.png" alt="twitter" />
       </a>
       <a
         target="_blank"
         href="https://www.linkedin.com/in/lucas-le-ray-89a61a135/"
         rel="noopener noreferrer"
       >
-        <img src="img/linkedin.png" alt="linkedin" />
+        <img src="/img/linkedin.png" alt="linkedin" />
       </a>
       <a
         target="_blank"
         href="https://www.facebook.com/lucas.leray.270"
         rel="noopener noreferrer"
       >
-        <img src="img/facebook.png" alt="facebook" />
+        <img src="/img/facebook.png" alt="facebook" />
       </a>
       <a
         target="_blank"
         href="https://github.com/LucasLeRay"
         rel="noopener noreferrer"
       >
-        <img src="img/github.png" alt="github" />
+        <img src="/img/github.png" alt="github" />
       </a>
       <a
         target="_blank"
         href="https://www.producthunt.com/@lucas_le_ray"
         rel="noopener noreferrer"
       >
-        <img src="img/producthunt.png" alt="product hunt" />
+        <img src="/img/producthunt.png" alt="product hunt" />
       </a>
       <a
         target="_blank"
         href="https://www.indiehackers.com/lucasleray"
         rel="noopener noreferrer"
       >
-        <img src="img/indiehacker.png" alt="indie hacker" />
+        <img src="/img/indiehacker.png" alt="indie hacker" />
       </a>
     </div>
   )
