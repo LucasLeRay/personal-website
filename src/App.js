dccc5c14230a73bd46dc80aa6af4149a3f03e362
Lucas Le Ray <lucas.a.leray@gmail.com>
2022-03-02 23:18:07 +0100

    Setup main page

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import me from './img/me.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Left">
+          <h1>I am<br /><b>Lucas Le Ray</b></h1>
+          <p>I'm a <b>Machine Learning Engineer</b> based in <b>London</b> and working at <a href="https://nibble.ai" target="_blank" rel="noopener noreferrer">nibble</a>. I'm passionate about <b>computational psychology</b> and <b>social sciences</b>.</p>
+          <div className="Links">
+            <div>
+              <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
+              <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
+            </div>
+            <div>
+              <a href="https://github.com/LucasLeRay" target="_blank" rel="noopener noreferrer">github</a>
+              <a href="https://lucas-le-ray.medium.com" target="_blank" rel="noopener noreferrer">articles</a>
+            </div>
+          </div>
+      </div>
+      <img src={me} />
+    </div>
+  );
+}
+
+export default App;
