6b036f91985d5dadbdbe0ce6d7067c40061b7c6a
Lucas Le Ray <lucas.a.leray@gmail.com>
2022-03-03 22:18:17 +0100

    Update links order

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
           <p>I'm a <b>Machine Learning Engineer</b> based in <b>London</b> and working at <a href="https://nibble.ai" target="_blank" rel="noopener noreferrer">nibble</a>. I'm passionate about <b>computational psychology</b> and <b>social sciences</b>.</p>
           <div className="Links">
             <div>
-              <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
+              <a href="https://lucas-le-ray.medium.com" target="_blank" rel="noopener noreferrer">articles</a>
               <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
             </div>
             <div>
               <a href="https://github.com/LucasLeRay" target="_blank" rel="noopener noreferrer">github</a>
-              <a href="https://lucas-le-ray.medium.com" target="_blank" rel="noopener noreferrer">articles</a>
+              <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
             </div>
           </div>
       </div>
