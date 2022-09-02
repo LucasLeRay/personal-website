972d442c47004d607f90d52de7f93969279a2662
Lucas Le Ray <lucas.a.leray@gmail.com>
2022-09-02 09:01:59 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ function App() {
   return (
     <div className="App">
       <div className="Left">
-          <h1>I am<br /><b>Lucas Le Ray</b></h1>
+          <h1>Lucas Le Ray</h1>
           <p>I'm a <b>Machine Learning Engineer</b> based in <b>London</b> and working at <a href="https://nibble.ai" target="_blank" rel="noopener noreferrer">nibble</a>. I'm passionate about <b>computational psychology</b> and <b>social sciences</b>.</p>
           <div className="Links">
-            <div>
-              <a href="https://lucas-le-ray.notion.site/Blog-8c05f99842a840fd9c73c2fa508437f8" target="_blank" rel="noopener noreferrer">articles</a>
-              <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
-            </div>
-            <div>
-              <a href="https://github.com/LucasLeRay" target="_blank" rel="noopener noreferrer">github</a>
-              <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
-            </div>
+            <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
+            <a href="https://github.com/LucasLeRay" target="_blank" rel="noopener noreferrer">github</a>
+            <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
           </div>
       </div>
       <img src={profile_src} alt="Lucas Le Ray"/>
