cee1bf6199b8ac324de9ec58d3eb98c8d0dd352b
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-03-26 18:58:00 +0200

    Update design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-import profile_src from './img/me.jpeg';
+import profile_src from './img/me.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="Left">
-          <h1>Lucas Le Ray</h1>
-          <p>I'm a <b>Machine Learning Engineer</b> based in <b>London</b> and working at <a href="https://nibble.ai" target="_blank" rel="noopener noreferrer">nibble</a>. I'm passionate about <b>computational psychology</b> and <b>social sciences</b>.</p>
+        <div className="Name">
+          <h2>Lucas</h2>
+          <h1>Le Ray</h1>
+        </div>
+        <div className="Description">
+          <p>I am a <b>Machine Learning Engineer</b> & <b>MLOps Engineer</b> working at <a href="https://nibble.ai" target="_blank" rel="noopener noreferrer">nibble</a>. My main interests lie in the practical application of <b>algorithms</b> in the field of <b>social psychology</b>.</p>
+          <p className="Quote">“<i>Problems are inevitable. Problems are solvable.</i>”<span className="Author"> - D. Deutsch</span></p>
+        </div>
           <div className="Links">
+            <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
             <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
             <a href="https://github.com/LucasLeRay" target="_blank" rel="noopener noreferrer">github</a>
-            <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
           </div>
       </div>
       <img src={profile_src} alt="Lucas Le Ray"/>
