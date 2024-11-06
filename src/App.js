b450750702391baca9f45278b484c9beab865740
LucasLeRay <lucas.a.leray@gmail.com>
2024-11-06 09:03:07 +0100

    Improve description

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
           <h1>Le Ray</h1>
         </div>
         <div className="Description">
-          <p>I am an <b>A.I. engineer</b>, specializing in its application to psychometrics and sociology. I deploy A.I. systems that turn complex human patterns into actionable insights.</p>
+          <p><b>A.I. engineer</b> specializing in its application to psychology and sociology. I employ algorithmic processes to understand, measure, and analyze public opinion.</p>
           <p className="Quote">“<i>Problems are inevitable. Problems are solvable.</i>”<span className="Author"> - D. Deutsch</span></p>
         </div>
           <div className="Links">
             <a href="mailto:lucas.a.leray@gmail.com" rel="noopener noreferrer">email</a>
             <a href="https://twitter.com/Lucas_Le_Ray" target="_blank" rel="noopener noreferrer">twitter</a>
-            <a href="https://lucas-le-ray.notion.site/" target="_blank" rel="noopener noreferrer">articles</a>
+            <a href="https://lucas-le-ray.notion.site/Lucas-Le-Ray-1313818a7a5d80468438e6b7c4382e13" target="_blank" rel="noopener noreferrer">articles</a>
           </div>
       </div>
       <img src={profile_src} alt="Lucas Le Ray"/>
